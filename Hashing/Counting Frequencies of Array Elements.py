e523095bc614bc2529ff3c79931858cb098077de
Shivam Yadav <166048646+asphixyy@users.noreply.github.com>
2026-07-14 20:42:48 +0530

    Add frequency counting method in Solution class
    Implemented a method to count frequencies of array elements.

--- Hashing/Counting Frequencies of Array Elements.py
+++ Hashing/Counting Frequencies of Array Elements.py
@@ -1,1 +1,14 @@
-
+class Solution:
+    def countFrequencies(self, nums):
+        freq={}
+        for i in nums:
+            if i in freq:
+                freq[i]+=1
+            else:
+                freq[i]=1
+        result=[]
+        for key in freq:
+            result.append([key,freq[key]])
+        return result
+x=Solution()
+print(x.countFrequencies([1, 2, 2, 1, 3])
