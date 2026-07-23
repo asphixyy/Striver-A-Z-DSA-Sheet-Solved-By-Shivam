c84d81d7f51bc8075ed223e91b567cf1b56b94fa
Shivam Yadav <166048646+asphixyy@users.noreply.github.com>
2026-07-23 21:35:32 +0530

    Update Selection sort.py

--- Selection Sort/Selection sort.py
+++ Selection Sort/Selection sort.py
@@ -2,11 +2,11 @@ class Solution:
     def selectionSort(self, nums):
         n=len(nums)
         for i in range(n):
-            min_num=i
+            min_idx=i
             for j in range (i+1,n):
                 if nums[j] < nums[min_num] :
-                    min_num=j
-            nums[i],nums[min_num]=nums[min_num],nums[i]
+                    min_idx=j
+            nums[i],nums[min_idx]=nums[min_idx],nums[i]
         return nums
 x=Solution()
 print(x.selectionSort([7, 4, 1, 5, 3]))
