d7e8e850a91fe43caed0c87741bc542f96b397e8
Shivam Yadav <166048646+asphixyy@users.noreply.github.com>
2026-07-24 22:55:20 +0530

    Implement function to check if array is sorted

---
+++ check if array is sorted in ascending order/checkArray.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isSorted(self, nums):
+        n=len(nums)
+        for i in range(0,n-1):
+            if nums[i]>nums[i+1]:
+                return False
+        return True
+
+x=Solution()
+print(x.isSorted([1, 2, 3, 4, 5]))
