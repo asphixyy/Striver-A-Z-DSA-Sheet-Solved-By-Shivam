bfc6f83b1b783a8b89001029f609bc5ef632cd9f
Shivam Yadav <166048646+asphixyy@users.noreply.github.com>
2026-08-02 23:13:15 +0530

    Update 0189-rotate-array.py

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -5,3 +5,16 @@ class Solution:
         nums.reverse()
         nums[:k] = reversed(nums[:k])
         nums[k:] = reversed(nums[k:])
+
+**Other approach**
+class Solution:
+    def rotate(self, nums, k):
+        n = len(nums)
+        end=[]
+        for i in range(k+1):
+           end.append(nums[i])
+        nums=nums[k+1:]
+        nums.extend(end)
+        return nums
+x=Solution()
+print(x.rotate( [1,2,3,4,5,6,7], 3))
