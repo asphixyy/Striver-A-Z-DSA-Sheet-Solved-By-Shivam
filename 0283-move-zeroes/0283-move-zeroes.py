313c1952172734bc9b0c634c96d7e5bef740810f
Shivam Yadav <166048646+asphixyy@users.noreply.github.com>
2026-08-03 23:56:09 +0530

    Time: 15 ms (11.72%), Space: 20.6 MB (24.02%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -0,0 +1,15 @@
+class Solution:
+    def moveZeroes(self, nums):
+        zero=[]
+        i=0
+        while i <(len(nums)):
+            if nums[i]==0:
+                zero.append(nums[i])
+                nums.pop(i)
+            else:
+                i+=1
+        nums.extend(zero)
+        return nums
+               
+x=Solution()
+print(x.moveZeroes([0, 0, 0, 1, 3, -2]))
