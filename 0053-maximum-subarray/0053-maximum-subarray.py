ed60ca25765b088bd0cff430571b891add09f6d0
Shivam Yadav <166048646+asphixyy@users.noreply.github.com>
2026-08-07 19:17:20 +0530

    Time: 15 ms (98.91%), Space: 31.4 MB (43.75%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -3,7 +3,12 @@ class Solution:
         current = best = nums[0]
 
         for num in nums[1:]:
-            current = max(num, current + num)
-            best = max(best, current)
+            if current < 0:
+                current = num
+            else:
+                current += num
+
+            if current > best:
+                best = current
 
         return best
