b8de14294813e9abeaf76375ee00527dc9e63545
Shivam Yadav <166048646+asphixyy@users.noreply.github.com>
2026-09-04 22:41:00 +0530

    Update 1903-largest-odd-number-in-string.py

--- 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.py
+++ 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.py
@@ -5,3 +5,17 @@ class Solution:
                 return num[:i+1]
 
         return ""
+
+# OTHER APPROACH
+class Solution:
+    def largestOddNumber(self, num):
+        if num[0]=="0":
+           num= num[1:]
+        for i in range(len(num),0,-1):
+            if int(num[len(num)-1])%2==0:
+                num=num[:-1]
+        return (num)
+        
+
+x=Solution()
+print(x.largestOddNumber("0214638"))
