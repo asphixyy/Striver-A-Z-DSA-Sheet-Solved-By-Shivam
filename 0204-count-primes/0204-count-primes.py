3d024e4e66751be64c871e669ef33f3b61cb39b5
Shivam Yadav <166048646+asphixyy@users.noreply.github.com>
2026-09-23 23:14:11 +0530

    Time: 1243 ms (81.02%), Space: 30.4 MB (97.18%) - LeetHub

--- 0204-count-primes/0204-count-primes.py
+++ 0204-count-primes/0204-count-primes.py
@@ -3,14 +3,17 @@ class Solution:
         if n <= 2:
             return 0
 
-        prime = [True] * n
-        prime[0] = prime[1] = False
+        prime = bytearray(b'\x01') * n
+        prime[0] = prime[1] = 0
 
         i = 2
 
         while i * i < n:
             if prime[i]:
-                prime[i * i:n:i] = [False] * (((n - 1 - i * i) // i) + 1)
+                start = i * i
+                count = (n - 1 - start) // i + 1
+                prime[start:n:i] = b'\x00' * count
+
             i += 1
 
         return sum(prime)
